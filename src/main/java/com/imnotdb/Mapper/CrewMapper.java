cac701bca8498835284e0f3d4476ea3afa66b08e
Chains.Z <10195101497@stu.ecnu.edu.cn>
2021-05-30 23:38:34 +0800

    table Crew add fulltext

--- src/main/java/com/imnotdb/Mapper/CrewMapper.java
+++ src/main/java/com/imnotdb/Mapper/CrewMapper.java
@@ -1,5 +1,40 @@
 package com.imnotdb.Mapper;
 
-public class CrewMapper {
+import com.imnotdb.Entity.Crew;
+import com.imnotdb.Entity.Crew;
+import com.imnotdb.Utils.NutDaoUtils;
+import org.nutz.dao.Cnd;
+import org.nutz.dao.QueryResult;
+import org.nutz.dao.impl.NutDao;
+import org.nutz.dao.pager.Pager;
+import org.nutz.dao.sql.Sql;
 
+import java.util.List;
+
+public class CrewMapper {
+    private static final NutDao dao = NutDaoUtils.getNutDao();
+    public QueryResult getCrewByDirectorsId(String name, int pageNumber, int pageSize, boolean setTotal){
+        Pager pager = dao.createPager(pageNumber, pageSize);
+        Sql sql = NutDaoUtils.matchAgainst(Crew.class, "directors", name, false);
+        sql.setPager(pager);
+        dao.execute(sql);
+        List<Crew> crewList = sql.getList(Crew.class);
+
+        if (setTotal){
+            Sql sqlCount = NutDaoUtils.matchAgainst(Crew.class, "title", name, true);
+            dao.execute(sqlCount);
+            pager.setRecordCount(sqlCount.getInt());
+        }
+        return new QueryResult(crewList, pager);
+    }
+    public QueryResult getCrewByTconst(String tconst, int pageNumber, int pageSize, boolean setTotal) {
+        Pager pager = dao.createPager(pageNumber, pageSize);
+        List<Crew> crewList = dao.query(Crew.class,
+                Cnd.where("tconst", "=", tconst),
+                pager);
+        if(setTotal){
+            pager.setRecordCount(dao.count(Crew.class, Cnd.where("tconst", "=", tconst)));
+        }
+        return new QueryResult(crewList, pager);
+    }
 }
